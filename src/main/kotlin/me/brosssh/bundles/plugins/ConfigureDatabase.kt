312ecfb2bb4aef4bdbde99c7466217dbc92188f4
brosssh <44944126+brosssh@users.noreply.github.com>
2025-12-16 17:11:28 +0100

    .

--- src/main/kotlin/me/brosssh/bundles/plugins/ConfigureDatabase.kt
+++ src/main/kotlin/me/brosssh/bundles/plugins/ConfigureDatabase.kt
@@ -1,7 +1,10 @@
 package me.brosssh.bundles.plugins
 
 import io.ktor.server.application.*
+import me.brosssh.bundles.db.tables.*
 import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 fun Application.configureDatabase() {
     val dbConfig = environment.config.config("app.db")
@@ -9,5 +12,9 @@ fun Application.configureDatabase() {
     val dbUser = dbConfig.property("user").getString()
     val dbPassword = dbConfig.property("password").getString()
 
-    Database.connect(jdbcUrl, driver = "org.postgresql.Driver", user = dbUser, password = dbPassword)
+    val db = Database.connect(jdbcUrl, driver = "org.postgresql.Driver", user = dbUser, password = dbPassword)
+
+    transaction(db) {
+        SchemaUtils.create(BundleTable, PackageTable, PatchTable, RefreshJobTable, SourceTable)
+    }
 }
